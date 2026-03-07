23bef8cf1e9148923c2abcb4cbbc1df3495947f0
abhincode826 <abhincode826@email.com>
2026-03-07 17:42:04 +0530

    fix: update anykernel.sh to use uppercase AnyKernel3 variables

--- AnyKernel13/anykernel.sh
+++ AnyKernel13/anykernel.sh
@@ -1,7 +1,6 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 # Adapted for Samsung Galaxy A20 (SM-A205F)
-
 properties() { '
 kernel.string=Eureka Kernel + KernelSU-Next by Abhin
 do.devicecheck=1
@@ -16,25 +15,13 @@ device.name4=SM-A205FN
 device.name5=SM-A205G
 '; }
 
-if [ -e /dev/block/platform/13500000.dwmmc0/by-name/BOOT ]; then
-    block=/dev/block/platform/13500000.dwmmc0/by-name/BOOT;
-elif [ -e /dev/block/platform/13500000.dwmmc0/by-name/boot ]; then
-    block=/dev/block/platform/13500000.dwmmc0/by-name/boot;
-fi
-is_slot_device=0;
-ramdisk_compression=auto;
+BLOCK=/dev/block/platform/13500000.dwmmc0/by-name/boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
 . tools/ak3-core.sh;
-
 split_boot;
 ui_print "- Installing Eureka Kernel + KernelSU-Next";
 flash_boot;
-ui_print " ";
-ui_print "- Installing DTB";
-flash_dtb;
-ui_print " ";
-ui_print "========================================";
-ui_print "  Samsung Galaxy A20 (SM-A205F)";
-ui_print "  Eureka Kernel + KernelSU-Next";
-ui_print "  Flash complete! Reboot to verify.";
-ui_print "========================================";
+ui_print "- Done!";
