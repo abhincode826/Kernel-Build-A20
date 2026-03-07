a2db375f71ca3d9af8656adec13a7b34454689be
abhincode826 <abhincode826@email.com>
2026-03-07 13:10:15 +0530

    fix: rewrite anykernel.sh with correct Samsung A20 block path and split_boot method

--- AnyKernel13/anykernel.sh
+++ AnyKernel13/anykernel.sh
@@ -2,9 +2,8 @@
 # osm0sis @ xda-developers
 # Adapted for Samsung Galaxy A20 (SM-A205F)
 
-## AnyKernel setup
 properties() { '
-kernel.string=Eureka Kernel + KernelSU-Next by YOUR_NAME
+kernel.string=Eureka Kernel + KernelSU-Next by Abhin
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -17,35 +16,25 @@ device.name4=SM-A205FN
 device.name5=SM-A205G
 '; }
 
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see ./tools/ak3-core.sh
+if [ -e /dev/block/platform/13500000.dwmmc0/by-name/BOOT ]; then
+    block=/dev/block/platform/13500000.dwmmc0/by-name/BOOT;
+elif [ -e /dev/block/platform/13500000.dwmmc0/by-name/boot ]; then
+    block=/dev/block/platform/13500000.dwmmc0/by-name/boot;
+fi
+is_slot_device=0;
+ramdisk_compression=auto;
+
 . tools/ak3-core.sh;
 
-## Samsung-specific boot image setup
-# A20 uses Samsung's proprietary boot image format; we flash the kernel
-# directly into the boot partition using Samsung's naming convention.
-
-## Dump/patch/restore boot image
-dump_boot;
-
-## Flash the kernel image
-# For Samsung Exynos devices the kernel image slot is 'kernel'
-write_boot;
-
-## Optionally flash DTB
-if [ -f "$ZIPFILE_DIR/dtb.img" ]; then
-    flash_dtb dtb.img;
-fi
-
-## End
-
+split_boot;
+ui_print "- Installing Eureka Kernel + KernelSU-Next";
+flash_boot;
+ui_print " ";
+ui_print "- Installing DTB";
+flash_dtb;
 ui_print " ";
 ui_print "========================================";
 ui_print "  Samsung Galaxy A20 (SM-A205F)";
 ui_print "  Eureka Kernel + KernelSU-Next";
+ui_print "  Flash complete! Reboot to verify.";
 ui_print "========================================";
-ui_print " ";
-ui_print "  Flash complete!";
-ui_print "  Reboot and open KernelSU-Next manager";
-ui_print "  to verify root status.";
-ui_print " ";
