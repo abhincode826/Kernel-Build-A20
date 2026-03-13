5245cd4bca3d1a2f600782363161bb08d4ca7f9e
abhincode826 <abhincode826@email.com>
2026-03-13 23:59:21 +0530

    ci: add kobject checkpoints throughout kernelsu_init

--- debug_patch.py
+++ debug_patch.py
@@ -3,14 +3,47 @@ with open('drivers/kernelsu/ksu.c', 'r') as f:
 
 c = c.replace(
     'int __init kernelsu_init(void)\n{',
-    '''int __init kernelsu_init(void)
-{
-\tstruct file *f;
-\tf = filp_open("/data/local/tmp/ksu_init.txt", O_WRONLY|O_CREAT|O_TRUNC, 0644);
-\tif (!IS_ERR(f)) {
-\t\tkernel_write(f, "KSU_INIT_CALLED\\n", 16, 0);
-\t\tfilp_close(f, NULL);
-\t}'''
+    'int __init kernelsu_init(void)\n{\n\tkobject_create_and_add("ksu_1_called", kernel_kobj);\n'
+)
+c = c.replace(
+    '    ksu_cred = prepare_creds();\n    if (!ksu_cred) {\n        pr_err("prepare cred failed!\\n");\n    }',
+    '    ksu_cred = prepare_creds();\n    if (!ksu_cred) {\n        pr_err("prepare cred failed!\\n");\n    }\n\tkobject_create_and_add("ksu_2_cred", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_feature_init();',
+    '\tksu_feature_init();\n\tkobject_create_and_add("ksu_3_feature", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_supercalls_init();',
+    '\tksu_supercalls_init();\n\tkobject_create_and_add("ksu_4_supercalls", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_syscall_hook_manager_init();',
+    '\tksu_syscall_hook_manager_init();\n\tkobject_create_and_add("ksu_5_syscall", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_lsm_hook_init();',
+    '\tksu_lsm_hook_init();\n\tkobject_create_and_add("ksu_6_lsm", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_allowlist_init();',
+    '\tksu_allowlist_init();\n\tkobject_create_and_add("ksu_7_allowlist", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_throne_tracker_init();',
+    '\tksu_throne_tracker_init();\n\tkobject_create_and_add("ksu_8_throne", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_ksud_init();',
+    '\tksu_ksud_init();\n\tkobject_create_and_add("ksu_9_ksud", kernel_kobj);'
+)
+c = c.replace(
+    '\tksu_file_wrapper_init();',
+    '\tksu_file_wrapper_init();\n\tkobject_create_and_add("ksu_10_filewrapper", kernel_kobj);'
+)
+c = c.replace(
+    '\treturn 0;\n}\nextern void ksu_observer_exit',
+    '\tkobject_create_and_add("ksu_11_done", kernel_kobj);\n\treturn 0;\n}\nextern void ksu_observer_exit'
 )
 
 with open('drivers/kernelsu/ksu.c', 'w') as f:
